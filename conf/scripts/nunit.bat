57ae343d8840574959d0837ebea2fa574fd584b2
ayoubensalem <ayoubensalem@gmail.com>
2019-09-23 21:23:37 +0100

    udpates

--- conf/scripts/nunit.bat
+++
@@ -1,6 +0,0 @@
-set pathNUnitExe="C:\Program Files (x86)\NUnit 2.7.1\bin\nunit-console.exe"
-
-set pathProjectToTest=%1
-set projectToTestName=%2
-cd %pathProjectToTest%
-%pathNUnitExe% %projectToTestName%
--- conf/scripts/sonar.bat
+++
@@ -1,8 +0,0 @@
-set pathMSBuild="C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
-set pathSonarQubeExe="C:\jenkins\sonar-scanner-msbuild\SonarScanner.MSBuild.exe"
-set solutionName=%1
-
-%pathSonarQubeExe% begin /k:"com.company.blogengine" /n:"BlogEngine" /v:"1.0.0" /d:sonar.host.url="http://67.207.94.110:9000" /d:sonar.login="admin" /d:sonar.password="admin" 
-%pathMSBuild% %solutionName% /t:rebuild /v:q /nologo
-%pathSonarQubeExe% end /d:sonar.login="admin" /d:sonar.password="admin"
-
